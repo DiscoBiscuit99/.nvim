1202763734881b48c484d8feba577c67a10a711c
DiscoBiscuit99 <discobiscuit31@gmail.com>
2023-08-23 03:45:40 +0200

    initial upload

---
+++ lua/plugins/configs/treesitter.lua
@@ -0,0 +1,42 @@
+local options = {
+  ensure_installed = {
+    "c",
+    "c_sharp",
+    "cpp",
+    "css",
+    "glsl",
+    "go",
+    "haskell",
+    "html",
+    "java",
+    "javascript",
+    "json",
+    "latex",
+    "lua",
+    "markdown",
+    "markdown_inline",
+    "mermaid",
+    "python",
+    "rust",
+    "scss",
+    "sql",
+    "svelte",
+    "sxhkdrc",
+    "toml",
+    "typescript",
+    "vim",
+    "wgsl",
+    "wgsl_bevy",
+    "yaml",
+    "zig",
+  },
+
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  },
+
+  indent = { enable = true },
+}
+
+return options
